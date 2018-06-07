ce7ebd1971495f7d68f493456502202be30ef4b0
Nicholas Nagrodski <nagrodski@gmail.com>
2018-06-06 22:48:54 -0400

    Create Notes.c

---
+++ Notes.c
@@ -0,0 +1,17 @@
+20180606 - Nicholas Nagrodski
+
+Took apart HP6328:
+1) Found most parts except bananaJacks
+2) Can't remove heatsing to sandblast.... :(
+3) Designed Front panel in FPD.  Need to finish it up and order it tomorrow!
+
+Downloaded EAGLECAD
+Downloaded FREECAD
+
+
+//--------------------------------------
+Tomorrow
+
+Word on 3D enclosure for TFT LCD screen!
+-> DECIDE on Wiring for it!!!  (D0/D7+ETC or SPI+ETC
+
